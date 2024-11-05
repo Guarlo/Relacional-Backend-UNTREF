e508cb3ee970bdb58b1b58124350ca5765ad7c37
Guarlo <xguarlo@gmail.com>
2024-11-05 00:47:30 -0300

    Actualización al 04-11-2024 (1)

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const dotenv = require('dotenv')
 const ENV = process.env.NODE_ENV || 'local'
 dotenv.config({ path: `.env.${ENV}` })
-if (ENV === 'local' || ENV === 'local_guarloweb' || ENV === 'local_railway') {
-  console.log(`.env.${ENV}`)
-  console.log(`host: ${process.env.DB_HOST}`)
-}
+//if (ENV === 'local' || ENV === 'local_guarloweb' || ENV === 'local_railway') {
+//  console.log(`.env.${ENV}`)
+//  console.log(`host: ${process.env.DB_HOST}`)
+//}
 
 const morgan = require('morgan')
 const express = require('express')
